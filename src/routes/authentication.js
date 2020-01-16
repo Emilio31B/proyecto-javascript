b7fe0a0ab7736394345c6c5bfe40305aaaa5a7cf
EmilioB <20161839@aloe.ulima.edu.pe>
2020-01-16 16:27:31 -0500

    falta postgres

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { isLoggenIn } = require('../lib/auth');
+const { isNotLoggenIn } = require('../lib/auth');
 
+//router.get('/signup', isNotLoggenIn, (req, res) =>{
 router.get('/signup', (req, res) =>{
     res.render('signup');
 });
@@ -16,12 +18,14 @@ router.get('/signup', (req, res) =>{
 });*/
 
 
+//router.post('/signup', isNotLoggenIn,passport.authenticate('local.signup',{
 router.post('/signup', passport.authenticate('local.signup',{
     successRedirect: '/profile',
     failureRedirect: '/signup'  
 }));
 
-router.get('/signin', (req, res) => {
+//router.get('/signin', isNotLoggenIn,(req, res) => {
+router.get('/signin',(req, res) => {
     res.render('signin');
 });
 
@@ -32,10 +36,12 @@ router.post('/signin',(req, res,next) => {
     })(req, res, next);
 });
 
-router.get('/profile', isLoggenIn,(req, res) => {
+//router.get('/profile', isLoggenIn,(req, res) => {
+router.get('/profile',(req, res) => {
     res.render('profile');
 });
 
+//router.get('/logout', isNotLoggenIn,(req, res) => {
 router.get('/logout', (req, res) => {
     req.logOut();
     res.redirect('/signin');
--- src/routes/links.js
+++ src/routes/links.js
@@ -2,41 +2,51 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../configs/database');
+const {isLoggenIn} = require('../lib/auth');
 
-router.get('/add', (req, res) =>{
+//router.get('/add', isLoggenIn ,(req, res) =>{
+router.get('/add',(req, res) =>{
     res.render('add');
 });
 
+//router.post('/add', isLoggenIn, async (req,res) =>{
 router.post('/add', async (req,res) =>{
     const {tittle, url, description } = req.body;
     const newlink = {
         tittle,
         url,
-        description
+        description,
+        //user_id: req.user.id
     };
     await pool.query('insert into links set ?',[newlink]);
     res.redirect('/links');
 });
 
+//router.get('/', isLoggenIn,async(req,res)=>{
 router.get('/',async(req,res)=>{
     const links = await pool.query('select * from links');
+    //const links = await pool.query('select * from links where user_id = ?',[req.user.id]);
     console.log(links);
     res.render('list', {links});
 });
 
+//router.get('/delete/:id', isLoggenIn,async (req, res) => {
 router.get('/delete/:id',async (req, res) => {
     const { id } = req.params;
     await pool.query('delete from links where id = ?', [id]);
     res.redirect('/links');
 });
-router.get('/edit/:id', async(req, res) => {
+
+//router.get('/edit/:id', isLoggenIn,async(req, res) => {
+router.get('/edit/:id',async(req, res) => {
     const { id } = req.params;
     const links = await pool.query('select * from links where id = ?',[id]);
     res.render('edit',{link: links[0]});
 
 });
 
-router.post('/edit/:id', async (req, res) => {
+//router.post('/edit/:id', isLoggenIn,async (req, res) => {
+router.post('/edit/:id',async (req, res) => {
     var { id }  = req.params;
     id = id.substring(1);
     id = parseInt(id);
